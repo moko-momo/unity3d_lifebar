6297294a165257ea9e3d1eb2930d9710877b7542
moko-momo <958465234@qq.com>
2018-06-05 17:42:12 +0800

    first commit

---
+++ Assets/IMGUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMGUI : MonoBehaviour {
+
+	private Rect BloodBar;
+	private Rect AddButton;
+	private Rect SubButton;
+	public float bloodValue = 1.0f;
+
+	void Start()
+	{
+
+		BloodBar = new Rect(500, 20, 200, 20);  	 
+		AddButton = new Rect(500, 50, 40, 20);  
+		SubButton = new Rect(650, 50, 40, 20); 
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(AddButton, "+"))
+		{
+			bloodValue += 0.1f;
+		}
+		if (GUI.Button(SubButton, "-"))
+		{
+			bloodValue -= 0.1f;
+		}
+		if (bloodValue > 1.0f)
+		{
+			bloodValue = 1.0f;
+		}
+		if (bloodValue < 0.0f)
+		{
+			bloodValue = 0.0f;
+		}
+
+
+		bloodValue = Mathf.Lerp(bloodValue, bloodValue, 0.05f);
+
+		GUI.HorizontalScrollbar(BloodBar, 0.0f, bloodValue, 0.0f, 1.0f);  
+	}
+}
